d1d9a06b4a31702511f9c146cdc5eaa3679ed120
d3adrabbit <49115041+d3adrabbit@users.noreply.github.com>
2022-12-10 22:44:47 +0800

    3 files modified, 1 added, 1 deleted

--- src/App.tsx
+++ src/App.tsx
@@ -8,43 +8,60 @@ function App() {
   const controlRef = useRef<any>(null);
   const plusRef = useRef<HTMLDivElement>(null);
 
+  const [num, setNum] = useState(0);
+
   const handleClick = () => {
-    gsap.from(plusRef.current, {
-      y: 20,
+    gsap
+      .timeline({})
+      .to(plusRef.current, {
+        y: -20,
+        opacity: 1,
+      })
+      .to(
+        plusRef.current,
+        {
+          opacity: 0,
+        },
+        0.3
+      );
+
+    gsap.set(plusRef.current, {
+      y: 0,
     });
 
-    console.log(12);
+    setNum(num + 1);
   };
 
-  // useLayoutEffect(() => {
-
-  //   gsap
-
-  // }, []);
+  useLayoutEffect(() => {
+    gsap.set(plusRef.current, {
+      y: 0,
+      opacity: 0,
+    });
+  }, []);
 
   return (
-    <div className="App" id="canvas">
-      <div
-        ref={plusRef}
-        className="absolute left-1/2 top-1/4 text-2xl font-bold"
-      >
-        + 1
+    <div>
+      <div className="p-5 text-2xl text-white font-bold flex">
+        <div>功德 {num}</div>
+        <div ref={plusRef} className="font-normal ml-5 ml-auto">
+          + 1
+        </div>
       </div>
       <Canvas
         style={{
           position: 'fixed',
           top: 0,
-          zIndex: 20,
           left: 0,
         }}
       >
         <Suspense fallback={null}>
-          <Stage controls={controlRef} intensity={0.8}>
+          <Stage controls={controlRef} intensity={0.2}>
             <Model onClick={handleClick} />
           </Stage>
         </Suspense>
 
         <OrbitControls
+          enableZoom={false}
           ref={controlRef}
           minPolarAngle={Math.PI / 2}
           maxPolarAngle={Math.PI / 2}
